87133514802aa34b75116d34412b561e680279c5
Wardan10 <iamlost123457@gmail.com>
2025-08-08 00:13:19 +0530

    added angle between player and other players

--- src/include/game_math.h
+++ src/include/game_math.h
@@ -1,10 +1,10 @@
-#include<math.h>
+#include<cmath>
 #include<SFML/System.hpp>
 double dist(float ax, float ay, float bx, float by);
 
 bool inside_map(sf::Vector2f final_pos,int map_unit,sf::Vector2f player_size);
 
-std::pair<std::pair<int, bool>, sf::Vector2f> find_end(sf::Vector2f start, float player_angle_yaw, int map_unit);
+std::pair<std::pair<float, bool>, sf::Vector2f> find_end(sf::Vector2f start, float player_angle_yaw, int map_unit);
 
-
+double angleFromAToB(float a_x, float a_y, float b_x, float b_y);
 
