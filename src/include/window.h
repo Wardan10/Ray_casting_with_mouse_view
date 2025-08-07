04edc404452ec4cdd6f831685c81895a3e7d3927
Wardan10 <iamlost123457@gmail.com>
2025-08-08 00:13:47 +0530

    adding new game_window

--- src/include/window.h
+++ src/include/window.h
@@ -1,14 +1,14 @@
-#pragma once
 #include<SFML/Graphics.hpp>
 #include<vector>
-class Window
+class Window_manage
 {
-public:
-	sf::RenderWindow* window;
+private:
 	unsigned int framerate;
+public:
+	sf::RenderWindow* window_manage;
 	unsigned int map_unit;
-	Window(int h, int w, std::string title);
-	void change_framerate(unsigned int x);
+	Window_manage(u_int16_t h, u_int16_t w, std::string title);
 	void close_window();
+	void change_framerate(unsigned int x);
 };
 
